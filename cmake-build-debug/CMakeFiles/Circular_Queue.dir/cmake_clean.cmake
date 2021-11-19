7ce32d058647b581f64a8527b128db3d609e5c7e
umair <umairliaqat552@gmail.com>
2021-11-20 03:41:34 +0500

    This the Queue Code very simple and Totally working

---
+++ cmake-build-debug/CMakeFiles/Circular_Queue.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Circular_Queue.dir/main.cpp.obj"
+  "Circular_Queue.exe"
+  "Circular_Queue.exe.manifest"
+  "Circular_Queue.pdb"
+  "libCircular_Queue.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Circular_Queue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
